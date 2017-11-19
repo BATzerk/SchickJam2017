f2098dee154b77e48d51f7e4a8a4570510c2b9d5
Andrew Garrahan <andrew@computerlunch.com>
2017-11-19 17:38:31 -0500

    rotated world

--- Assets/RotateRoom.cs
+++ Assets/RotateRoom.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class RotateRoom : MonoBehaviour {
 
-	float speed = 10f;
+	float speed = 1f;
 
 	void Update()
 	{
 		// Rotate the object around its local X axis at 1 degree per second
-		transform.Rotate(Vector3.right * Time.deltaTime);
+	//	transform.Rotate(Vector3.right * Time.deltaTime);
 
 		// ...also rotate around the World's Y axis
-		transform.Rotate(Vector3.up * Time.deltaTime * speed, Space.World);
+		transform.Rotate(Vector3.forward * Time.deltaTime * speed, Space.World);
 	}
 }
